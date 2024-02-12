e290b230912726cdef204b5f5dec40ae6728c88b
yang <cryptopunkyang@pm.me>
2024-02-12 22:42:28 +0100

    feat: add init command

--- cmd/wails2/dev.go
+++ cmd/wails2/dev.go
@@ -5,13 +5,17 @@ package main
 import (
 	"github.com/cryptopunkscc/go-astral-js/wails/build"
 	"github.com/cryptopunkscc/go-astral-js/wails/bundle"
+	"github.com/cryptopunkscc/go-astral-js/wails/create"
+	"github.com/cryptopunkscc/go-astral-js/wails/create/templates"
 	"github.com/cryptopunkscc/go-astral-js/wails/dev"
 	"github.com/leaanthony/clir"
+	"github.com/pterm/pterm"
 	"log"
 )
 
 func main() {
 	cli := clir.NewCli("AstralJS", "JavaScript development environment for Astral.", "0.0.1")
+	cli.NewSubCommandFunction("init", "Create production bundle.", cliInit)
 	cli.NewSubCommandFunction("dev", "Run development server for given dir.", cliDevelopment)
 	cli.NewSubCommandFunction("run", "Execute app from bundle, dir, or file.", cliApplication)
 	cli.NewSubCommandFunction("build", "Build application.", cliBuild)
@@ -35,6 +39,39 @@ func cliBuild(f *FlagsBuild) error {
 
 type FlagsBundle struct{ FlagsBuild }
 
-func cliBundle(f *FlagsBundle) (err error) {
+func cliBundle(f *FlagsBundle) error {
 	return bundle.Create(f.Path)
+}
+
+type FlagsInit struct {
+	Template string `name:"t" description:"Name of built-in template to use, path to template or template url"`
+	Name     string `name:"n" description:"Name of project"`
+	Dir      string `name:"d" description:"Project directory"`
+	Force    bool   `name:"f" description:"Force recreate project"`
+	List     bool   `name:"l" description:"List available templates"`
+}
+
+func cliInit(f *FlagsInit) error {
+	if f.List {
+		return cliList()
+	} else {
+		return create.Run(f.Name, f.Dir, f.Template, f.Force)
+	}
+}
+
+func cliList() error {
+	templateList, err := templates.List()
+	if err != nil {
+		return err
+	}
+
+	pterm.DefaultSection.Println("Available templates")
+
+	table := pterm.TableData{{"Template", "Short Name", "Description"}}
+	for _, template := range templateList {
+		table = append(table, []string{template.Name, template.ShortName, template.Description})
+	}
+	err = pterm.DefaultTable.WithHasHeader(true).WithBoxed(true).WithData(table).Render()
+	pterm.Println()
+	return err
 }
--- wails/run.go
+++
@@ -1,5 +0,0 @@
-package wails
-
-import "github.com/wailsapp/wails/v2/pkg/options"
-
-type Run func(path string, opt *options.App) error
